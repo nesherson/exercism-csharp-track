ffe1b7623faedce644d7e5cd0f68e5a27d7cebcf
nesherson <nesad.omeragich27@gmail.com>
2021-09-29 20:43:01 +0200

    Finish exercise with all tests passed

--- lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,15 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutesSpent) => ExpectedMinutesInOven() - minutesSpent;
+
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutesSpent) => PreparationTimeInMinutes(layers) + minutesSpent;
 }
