759ccadbe2577bdfb09083a40040f44dda1bb885
nesherson <nesad.omeragich27@gmail.com>
2021-10-01 01:36:00 +0200

    Implement first method

--- interest-is-interesting/InterestIsInteresting.cs
+++ interest-is-interesting/InterestIsInteresting.cs
@@ -4,7 +4,14 @@ static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        if (balance < 0)
+            return 3.213f;
+        else if (balance < 1000)
+            return 0.5f;
+        else if (balance >= 1000 && balance < 5000)
+            return 1.621f;
+        else if (balance >= 5000)
+            return 2.475f;
     }
 
     public static decimal Interest(decimal balance)
