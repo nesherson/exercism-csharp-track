2a28d80bb73071c2a0ea00d6be4736f4e52cfcbd
nesherson <nesad.omeragich27@gmail.com>
2021-10-14 00:04:55 +0200

    Finish exercise with all tests passed

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -4,6 +4,9 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        long gigasecond = 1000000000;
+        long seconds = gigasecond * 1000;
+
+        return moment.AddMilliseconds(seconds);
     }
 }
