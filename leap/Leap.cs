21969b201ebdaea47e1a93f5c821972fdd9ceaaf
nesherson <nesad.omeragich27@gmail.com>
2021-10-16 00:25:39 +0200

    Finish exercise with all test passed

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -4,6 +4,13 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (year % 4 != 0)
+            return false;
+        else if (year % 100 != 0)
+            return true;
+        else if (year % 400 != 0)
+            return false;
+
+        return true;
     }
 }
