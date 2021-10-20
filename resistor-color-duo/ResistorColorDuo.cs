d6c7166edd7a4115d1c413a13028c5278416f11b
nesherson <nesad.omeragich27@gmail.com>
2021-10-20 20:42:13 +0200

    Finish exercise with all tests passedd

--- resistor-color-duo/ResistorColorDuo.cs
+++ resistor-color-duo/ResistorColorDuo.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 public static class ResistorColorDuo
 {
     public static int Value(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Dictionary<string, char> BandColors = new();
+        BandColors.Add("black", '0');
+        BandColors.Add("brown", '1');
+        BandColors.Add("red", '2');
+        BandColors.Add("orange", '3');
+        BandColors.Add("yellow", '4');
+        BandColors.Add("green", '5');
+        BandColors.Add("blue", '6');
+        BandColors.Add("violet", '7');
+        BandColors.Add("grey", '8');
+        BandColors.Add("white", '9');
+
+        char[] charValues = new char[2];
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            charValues[i] = BandColors[colors[i]];
+        };
+
+        string strValue = new string(charValues);
+
+        Console.WriteLine(strValue);
+
+        return Int32.Parse(strValue);
     }
 }
