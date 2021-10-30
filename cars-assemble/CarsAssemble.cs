d9d585e747bf2bbc1ed9b95db6dbbc867001fa57
nesherson <nesad.omeragich27@gmail.com>
2021-10-30 23:34:30 +0200

    Finish exercise with all tests passed

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -4,16 +4,25 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        if (speed == 0)
+            return 0;
+        else if (speed >= 1 && speed <= 4)
+            return 1;
+        else if (speed >= 5 && speed <= 8)
+            return 0.9;
+        else if (speed == 9)
+            return 0.8;
+
+        return 0.77;
     }
-    
+
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        return (speed * 221) * SuccessRate(speed);
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        return Convert.ToInt32(Math.Floor(ProductionRatePerHour(speed) / 60));
     }
 }
