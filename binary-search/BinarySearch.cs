e2eabc398d9e102bf5ebaede2f9d1bcbad7bdf09
nesherson <nesad.omeragich27@gmail.com>
2021-11-04 23:54:43 +0100

    Finish exercise with all test passed

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,9 +1,49 @@
 using System;
-
 public static class BinarySearch
 {
+    public static int[] SortAscending(int[] arr)
+    {
+        int[] newArr = arr;
+
+        for (int i = 0; i < newArr.Length; i++)
+        {
+            for (int j = 0; j < newArr.Length; j++)
+            {
+                int temp = 0;
+                if (newArr[j] > newArr[i])
+                {
+                    temp = newArr[i];
+                    newArr[i] = newArr[j];
+                    newArr[j] = temp;
+                }
+            }
+        }
+        return newArr;
+    }
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int[] tempArr = input;
+
+        while (tempArr.Length > 0)
+        {
+            int index = tempArr.Length / 2;
+            int temp = tempArr[index];
+
+            if (tempArr.Length == 1 && temp != value)
+                return -1;
+
+            if (temp == value)
+                return Array.IndexOf(input, temp);
+
+            if (temp > value)
+                tempArr = new ArraySegment<int>(tempArr, 0, index).ToArray();
+
+            if (temp < value)
+                tempArr = new ArraySegment<int>(tempArr, index, tempArr.Length - index).ToArray();
+        }
+
+        return -1;
     }
 }
+
+
