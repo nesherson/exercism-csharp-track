e0006315cf31d177f98b45f613295c0bf003f163
nesherson <nesad.omeragich27@gmail.com>
2021-11-04 18:04:48 +0100

    Finish exercise with all tests passed

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,52 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!multiples.Any())
+            return 0;
+
+        List<int> tempMultiples = multiples.ToList();
+        int len = tempMultiples.Count;
+
+        int num = 1;
+        int index = 0;
+        int curr = 0;
+        List<int> uniques = new();
+
+        while (index < len)
+        {
+            int temp = 0;
+            do
+            {
+                curr = tempMultiples[index];
+
+                if (curr == 0)
+                {
+                    index++;
+                    break;
+                }
+
+                temp = curr * num;
+
+                if (temp >= max)
+                    break;
+
+                if (!uniques.Contains(temp))
+                {
+                    uniques.Add(temp);
+                }
+
+                num++;
+            } while (true);
+
+            num = 0;
+            index++;
+        }
+        return uniques.Sum();
     }
 }
+
