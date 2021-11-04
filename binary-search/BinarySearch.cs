9153d03c413e92fca783717008fd173ea1798dbc
nesherson <nesad.omeragich27@gmail.com>
2021-11-04 23:59:18 +0100

    Remove redundant code

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,25 +1,6 @@
 using System;
 public static class BinarySearch
 {
-    public static int[] SortAscending(int[] arr)
-    {
-        int[] newArr = arr;
-
-        for (int i = 0; i < newArr.Length; i++)
-        {
-            for (int j = 0; j < newArr.Length; j++)
-            {
-                int temp = 0;
-                if (newArr[j] > newArr[i])
-                {
-                    temp = newArr[i];
-                    newArr[i] = newArr[j];
-                    newArr[j] = temp;
-                }
-            }
-        }
-        return newArr;
-    }
     public static int Find(int[] input, int value)
     {
         int[] tempArr = input;
