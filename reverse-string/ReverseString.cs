b0b703116f272efd3fc7920a1b918b7654a95676
nesherson <nesad.omeragich27@gmail.com>
2021-11-20 23:17:47 +0100

    Finish exercise with all tests passed

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (input == "")
+            return "";
+
+        char[] tempInput = input.ToCharArray();
+        List<char> reversedString = new List<char>();
+
+        for (int i = tempInput.Length - 1; i >= 0; i--)
+        {
+            reversedString.Add(tempInput[i]);
+        }
+
+        return new string(reversedString.ToArray());
+
     }
 }
