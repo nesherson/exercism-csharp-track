4fc58abe03a04eed065337cb640871f390ed6293
nesherson <nesad.omeragich27@gmail.com>
2021-11-27 13:59:17 +0100

    Finish third task

--- tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
+++ tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -15,7 +15,9 @@ public static class Languages
 
     public static List<string> AddLanguage(List<string> languages, string language)
     {
-        throw new NotImplementedException("Please implement the static Languages.AddLanguage() method");
+        languages.Add(language);
+
+        return languages;
     }
 
     public static int CountLanguages(List<string> languages)
