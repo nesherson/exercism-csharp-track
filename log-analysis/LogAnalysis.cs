5929b897e261ba1358ba334938f72fe2f5c3f551
nesherson <nesad.omeragich27@gmail.com>
2021-12-02 15:49:21 +0100

    Initial exercise structure

---
+++ log-analysis/LogAnalysis.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class LogAnalysis
+{
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+
+    // public static string SubstringAfter()
+    // {
+
+    // }
+
+    // TODO: define the 'SubstringBetween()' extension method on the `string` type
+
+    // TODO: define the 'Message()' extension method on the `string` type
+
+    // TODO: define the 'LogLevel()' extension method on the `string` type
+}
