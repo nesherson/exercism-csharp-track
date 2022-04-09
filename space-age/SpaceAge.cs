453d889d9253f1492da6ef9ba806481f349aaeb5
nesherson <nesad.omeragich27@gmail.com>
2022-04-09 14:53:01 +0200

    Finish exercise with all tests passed

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -2,47 +2,78 @@ using System;
 
 public class SpaceAge
 {
+    private double _seconds;
     public SpaceAge(int seconds)
     {
+        _seconds = (double)seconds;
+    }
+
+    private double CalculateAge(double earthYears)
+    {
+        const double _earthDays = 365.25;
+        double earthDays = earthYears * _earthDays;
+
+        double ageInMinutes = _seconds / 60;
+        double ageInHours = ageInMinutes / 60;
+        double ageInDays = ageInHours / 24;
+        double age = ageInDays / earthDays;
+
+        return age;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYears = 1.0;
+
+        return CalculateAge(earthYears);
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnMercury = 0.2408467;
+
+        return CalculateAge(earthYearsOnMercury);
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnVenus = 0.61519726;
+
+        return CalculateAge(earthYearsOnVenus);
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnMars = 1.8808158;
+
+        return CalculateAge(earthYearsOnMars);
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnJupiter = 11.862615;
+
+        return CalculateAge(earthYearsOnJupiter);
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnSaturn = 29.447498;
+
+        return CalculateAge(earthYearsOnSaturn);
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnUranus = 84.016846;
+
+        return CalculateAge(earthYearsOnUranus);
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        const double earthYearsOnNeptune = 164.79132;
+
+        return CalculateAge(earthYearsOnNeptune);
     }
 }
