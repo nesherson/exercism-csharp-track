b72e3ad678d80f2561d52c2eaf23d6b1c78251c8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-06-04 21:17:16 +0200

    Finish flatten array exercise

--- flatten-array/FlattenArray.cs
+++ flatten-array/FlattenArray.cs
@@ -1,10 +1,31 @@
-using System;
 using System.Collections;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = new List<object>();
+
+        foreach (var item in input)
+        {
+            if (item is null)
+                continue;
+
+            if (item is IEnumerable enumerable)
+            {
+                var flattenArray = Flatten(enumerable);
+
+                foreach (var item2 in flattenArray)
+                {
+                    list.AddRange(item2);
+                }
+            }
+            else
+            {
+                list.Add(item);
+            }
+        }
+
+        return list;
     }
 }
